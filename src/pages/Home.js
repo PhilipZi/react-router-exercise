bbe41874e4cfb93f58c0afc56ecb336b73354e4c
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-03-17 16:44:48 +0100

    add navigation programmatically

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  const navigateHandler = () => {
+    navigate("./products");
+  };
+
   return (
     <>
       <h1>MY HOME PAGE</h1>
       <p>
         Go to <Link to="/products">the list of products</Link>.
+      </p>
+      <p>
+        <button onClick={navigateHandler}>Navigate to Products</button>
       </p>
     </>
   );
