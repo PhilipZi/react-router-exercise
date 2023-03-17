9197150ac5de206dd60e801e8fe0eebfab93643c
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-03-17 12:37:57 +0100

    add Root-component add outlet from react to root component

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -7,6 +7,8 @@ function MainNavigation() {
         <ul>
           <li>
             <Link to="/">Home</Link>
+          </li>
+          <li>
             <Link to="/products">Products</Link>
           </li>
         </ul>
