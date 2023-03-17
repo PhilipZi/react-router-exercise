045b8fa512a48ee15a9c7c142ac4fa6c19d79ba2
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-03-17 16:56:50 +0100

    add productDetail page add new path for router

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 
 import ErrorPage from "./pages/Error";
 import HomePage from "./pages/Home";
+import ProductDetail from "./pages/ProductDetail";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/Root";
 
@@ -25,6 +26,7 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <ProductsPage /> },
+      { path: "products/product-2", element: <ProductDetail /> },
     ],
   },
 ]);
---
+++ src/pages/ProductDetail.js
@@ -0,0 +1,4 @@
+function ProductDetail() {
+  return <h1>Product Details!</h1>;
+}
+export default ProductDetail;
