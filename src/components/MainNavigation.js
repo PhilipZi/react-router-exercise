90b6017d3d6f41ce46eb27e7cbfc18500c053208
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-03-17 12:27:47 +0100

    add main-navigation-component add Link to navigation

---
+++ src/components/MainNavigation.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+function MainNavigation() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+export default MainNavigation;
