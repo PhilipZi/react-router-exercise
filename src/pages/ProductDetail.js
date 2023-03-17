18f549d61d54ca8da2195cc37c0bd81df9eaa98c
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-03-17 17:46:16 +0100

    add realtive path to productdetailpage

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 function ProductDetail() {
   const params = useParams();
@@ -7,6 +7,11 @@ function ProductDetail() {
     <>
       <h1>Product Details!</h1>
       <p>{params.productId}</p>
+      <p>
+        <Link to=".." relative="path">
+          back
+        </Link>
+      </p>
     </>
   );
 }
